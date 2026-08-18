4265b05336ea0313d58258c673efd64ac0aa83f7
Jayesh-213 <jayeshkawade213@gmail.com>
2026-08-18 23:34:51 +0530

    Delete 02_BASIC_JOINS /197_Rising_Temperature.sql

--- 02_BASIC_JOINS /197_Rising_Temperature.sql
+++
@@ -1,6 +0,0 @@
-# Write your MySQL query statement below
-Select t.id
-From  Weather as t  # t = today temp, y = yestarday temp 
-JOIN Weather as y
-On t.recordDate = Date_ADD(y.recordDate , Interval 1 day)
-where t.temperature > y.temperature;
