f6cbafb271eb655c14d4126c5c2cdd97695c8ac0
Jayesh-213 <jayeshkawade213@gmail.com>
2026-08-30 21:56:21 +0530

    Add SQL queries for various problems in SQL-50 from leetcode

--- 03_Basic Aggregate Functions/1075_Project_Employees_I.sql
+++ 03_Basic Aggregate Functions/1075_Project_Employees_I.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below 
 
 SELECT p1.project_id, 
-    ROUND(AVG(e1.experience_years), 2) AS average_years 
+    ROUND(AVG(e1.experience_years), 2) AS average_years
 FROM Project AS p1 
 JOIN Employee AS e1 
     ON p1.employee_id = e1.employee_id 
